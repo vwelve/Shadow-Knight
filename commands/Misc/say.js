58fe9f4dfd9b108fa100f3f72c8291cee1a389c4
Dominic Humber <dhumber2017@gmail.com>
2019-10-29 17:01:14 -0400

    description to say command"

--- commands/Misc/say.js
+++ commands/Misc/say.js
@@ -3,6 +3,7 @@ module.exports = {
     alias: ["repeat"],
     accessLvl: "guild",
     permission: "VIEW_CHANNEL",
+    description: "Repeats what the user said.",
     run: async (client, msg, args) => {
         msg.channel.send(msg.content);
     }
